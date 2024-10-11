c1fb377a2f4770b648ec09505ea3db6a00150050
Steve Smith <steve.a.smith@avanade.com>
2024-10-11 17:39:02 +0100

    clean up

--- src/app/clientComponent.tsx
+++ src/app/clientComponent.tsx
@@ -1,14 +1,10 @@
 "use client"
 
-import { useState } from "react";
-
 export const ClientComponent = () => {
-    const [name, setName] = useState<string>();
     return (
         <section className="border p-5">
         <h2>This is a client component</h2>
-        <p>its using state see? Name is {name}</p>
-        <input type="text" onChange={(e) => setName(e.target.value)} />
+
         <ul className="mt-5">
           <li>BROWSER_VARIABLE: {process.env.NEXT_PUBLIC_BROWSER_VARIABLE}</li>
         </ul>
